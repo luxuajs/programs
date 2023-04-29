5a42e7c1b45fb8bfb6e49c92cfe05165ab41be58
Juan Sanabria <juansanabriaalfa08@gmail.com>
2023-04-29 11:30:02 -0400

    I've added comments to efficiente_queens.py file

--- python_programming/Backtracking/Queens_problem/efficient_queens.py
+++ python_programming/Backtracking/Queens_problem/efficient_queens.py
@@ -1,25 +1,31 @@
-
+# N represent the length of the solution array.
 N = 4
 
+# "solution" is the array where i'll put each solution.
 solution = [None] * N
 
+# This procedure print each solution.
 def see_queens(solution):
   for i in range(N):
     print(f"Q{i+1} ---> C{i+1} - R{solution[i]+1}")
   print("\n")
+
+# This function verify if a queen can be in a determine position
+#(It verify the row and diagonal)
 def free_queen(i, j, solution):
   #Var
   #Begin
   k = 0
   result = True
   while(k < j and result):
-    if(solution[k] == i):
+    if(solution[k] == i): # Verifying the row.
       result = False
-    if(abs(i - solution[k]) == abs(j - k)):
+    if(abs(i - solution[k]) == abs(j - k)): # Verifying the diagonal.
       result = False
     k += 1
   return result
 
+# Main funtion which use backtracking to solve the problem queens.
 def queens_on_table(j, solution):
   #Var
   #Begin
@@ -32,4 +38,6 @@ def queens_on_table(j, solution):
       else:
         queens_on_table(j+1, solution)
     i += 1
+
+# Calling to main function.
 queens_on_table(0,solution)
